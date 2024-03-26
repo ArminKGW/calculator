b4dfa999cc251a8181820f4d884fa0e07a9d97de
Armin <160379151+ArminKGW@users.noreply.github.com>
2024-03-26 13:53:52 +0100

    Complete "looks" part of calculator.
    The designing phase is complete, but nothing is
    interactive yet.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# calculator
+This project is about building a calculator with 4 basic operators.
+The calculations are done 1 by 1, meaning that 20 - 2 * 3 will not be equal to 14, but it will be (20-2) * 3 which is 54.
---
+++ script.js
@@ -0,0 +1,42 @@
+/*let firstNumber, secondNumber, operator;
+const numbers = document.querySelector(".num-op");
+numbers.addEventListener("click", (event) => {
+    let number = event.target.textContent;
+    console.log(number);
+});
+
+
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(a, b, operator){
+    switch(operator){
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case 'x':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+}
+*/
