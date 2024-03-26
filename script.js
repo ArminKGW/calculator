f873266e58b8c21e55fcaec0620443ee8c561317
Armin <160379151+ArminKGW@users.noreply.github.com>
2024-03-26 21:06:25 +0100

    Work with correct usage, cannot chain yet
    calculator works if used properly; '=' works after entering
    two numbers and an operator. does not work yet if an operator is
    entered instead of '='.
    For example, 2 + 8 = 10 will work, but 2 + 7 + 1 will have incorrect result.
    '.' can be used to make decimal numbers as well.
    Delete and AC buttons do not work yet.

--- script.js
+++ script.js
@@ -1,42 +1,93 @@
-/*let firstNumber, secondNumber, operator;
 const numbers = document.querySelector(".num-op");
+const display = document.querySelector(".display");
+let firstNumber = null, secondNumber = null, operator = null, inOperation = false, result;
 numbers.addEventListener("click", (event) => {
-    let number = event.target.textContent;
-    console.log(number);
+    if(event.target.className.includes("number")){
+        getNumber(event);
+    }
+    if(event.target.className.includes("operator")){
+        /*if(firstNumber && secondNumber){
+            console.log("yes and yes");
+            result = operate(firstNumber, secondNumber, operator);
+            display.textContent = result;
+            firstNumber = display.textContent;
+            secondNumber = null;
+        }*/
+        inOperation = true;
+        operator = getOperator(event);
+    }
+    if(event.target.className == "do"){
+        secondNumber = display.textContent;
+        result = operate(firstNumber, secondNumber, operator);
+        display.textContent = result;
+        firstNumber = display.textContent;
+        secondNumber = null;
+    }
 });
 
+function getOperator(event){
+    operator = event.target.textContent; 
+    if(firstNumber == null){
+        firstNumber = display.textContent;
+    }
+    else{
+        if(secondNumber == null){
+            secondNumber = display.textContent;
+        }       
+    }
+    return operator;
+}
+
+function getNumber(event){
+    let number = event.target.textContent; 
+    if(inOperation) {
+        display.textContent = '';
+        inOperation = false;
+    }
+    if(number === '.'){
+        if(!(display.textContent.includes('.'))){
+            display.textContent += number;
+        }
+    }
+    else if(display.textContent != '0'){
+        display.textContent += number;
+    }
+    else display.textContent = number;
+}
 
 
 function add(a, b){
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b){
-    return a - b;
+    return +a - +b;
 }
 
 function multiply(a, b){
-    return a * b;
+    return +a * +b;
 }
 
 function divide(a, b){
-    return a / b;
+    return +a / +b;
 }
 
 function operate(a, b, operator){
+    let result;
     switch(operator){
         case '+':
-            add(a, b);
+            result = add(a, b);
             break;
         case '-':
-            subtract(a, b);
+            result = subtract(a, b);
             break;
         case 'x':
-            multiply(a, b);
+            result = multiply(a, b);
             break;
         case '/':
-            divide(a, b);
+            result = divide(a, b);
             break;
     }
+    return result;
 }
-*/
+
